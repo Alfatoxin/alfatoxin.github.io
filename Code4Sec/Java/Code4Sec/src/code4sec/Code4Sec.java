eb7e3bc5572f19096e75f61833ad37ae30746a3f
Alfatoxin <48711154+Alfatoxin@users.noreply.github.com>
2021-04-04 18:01:51 +0700

    Update Code4Sec.java

--- Code4Sec/Java/Code4Sec/src/code4sec/Code4Sec.java
+++ Code4Sec/Java/Code4Sec/src/code4sec/Code4Sec.java
@@ -8,11 +8,21 @@ import java.util.Base64;
 public class Code4Sec {
 
     public static void main(String[] args) throws NoSuchAlgorithmException {
-        String text = "Sakarin Kaewsathitwong";
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        byte[] hash = digest.digest(text.getBytes(StandardCharsets.UTF_8));
-        String encoded = Base64.getEncoder().encodeToString(hash);
-        System.out.println("Encoded is " + encoded);
+//        String text = "Sakarin Kaewsathitwong";
+//        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+//        byte[] hash = digest.digest(text.getBytes(StandardCharsets.UTF_8));
+//        String encoded = Base64.getEncoder().encodeToString(hash);
+//        System.out.println("Encoded is " + encoded);
+
+        int a, b;
+        a = 5;
+        b = 100;
+        
+        a = b;
+        a = a + 10;
+        
+        System.out.println("a is " + a);
+        System.out.println("b is " + b);
     }
     
 }
